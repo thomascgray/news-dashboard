d24b5bc2c0a17da6a4c6ec9016b3becfbda17f99
Tom Gray <tom.c.gray@gmail.com>
2018-02-28 13:44:24 +0000

    investigate further into YQL

--- src/news_sources/rss.js
+++ src/news_sources/rss.js
@@ -6,12 +6,14 @@ class Rss {
     static fulfill(rssFeed) {
         return new Promise((resolve, reject) => {
             $(document).ready(function () {
+                // $(document).append('')
                 $.ajax({
-                    url: "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(rssFeed.meta.url),
+                    url: `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D%27${encodeURIComponent(rssFeed.meta.url)}%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
                     dataType: "jsonp",
                 }).fail((xhr, status, error) => {
                     return reject();
                 }).done(response => {
+                    console.log(response);
                     const links = response.items.map(item => {
                         return {
                             title: item.title,
